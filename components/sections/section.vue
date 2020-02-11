72c42a07aaf3b7585752dec6822b4e576f7dc25b
James Paik <jpaik456@gmail.com>
2020-02-11 17:05:11 -0500

    Consolidate looped sections

---
+++ components/sections/section.vue
@@ -0,0 +1,28 @@
+<template>
+    <section v-if="data.name">
+      <hgroup>
+        <h3>
+          {{data.name}}
+          <nav v-if="data.link">
+            <a :href="data.link.url" target="_blank" rel="noopener"><img src="/link-regular.svg" :alt="data.link.alt" /></a>
+          </nav>
+        </h3>
+        <h4>{{data.timeframe}}</h4>
+      </hgroup>
+      
+      <p>
+        {{data.position}}
+      </p>
+      <ul>
+        <li v-for="point in data.bullets" :key="point">
+            {{point}}
+        </li>
+      </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['data']
+}
+</script>
